a4185da34fb4b1d8090b8b5f77cb2841102abb70
victor magdesian <vicfemagdesian@gmail.com>
2022-10-10 14:59:55 -0300

    Banca final

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React , {useState, useEffect} from 'react'
 import './Card.css'
 
-export default function Card() {
+export default function Card(props) {
+
     return (
       <div className="Card">
-        <h6 className="Title">Dados da Última Viagem</h6>
+        <h6 className="Title">{props.title}</h6>
         <hr />
-        <p className="Description">Descrição Componente</p>
+        <p className="Label">Distancia percorrida:   {props.distancia}</p>
+        <p className="Label">{"Consumo médio(KM/L):   "+props.kml}</p>
+        <p className="Label">Autonomia combustível restante:   {props.conbustivelRestante}</p>
+        <p className="Label">Calibragem dos pneus:   {props.calibragem}</p>
+        <p className="Label">Troca de oleo:   {props.oleoTroca}</p>
+        <p className="Label">Aviso de revisão:   {props.revisao}</p>
       </div>
     );
   }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,52 @@
-import React from 'react'
+import React , {useState, useEffect} from 'react'
 import './Home.css'
 import AppBarDorf  from '../layout/AppBarDorf'
 import FooterDorf from '../layout/FooterDorf'
 import Card from '../components/Card'
+import api from "../../service/api.js"
 
 export const Home = () => {
+  const [data, setData] = useState(null);
+
+
+  useEffect(() => {
+    api
+      .get('/api')
+      .then(({data}) => {
+        setData(data)
+      })
+  }, []);
+
+  console.log(data)
+  
   return (
     <div className="Home">
         <AppBarDorf />
-        <Card />
+        <Card title={data.listaViajem[0].title} 
+              kml={data.listaViajem[0].consumoMedio + "KM"} 
+              revisao={"Em "+ data.listaViajem[0].revisao + " Dias"} 
+              distancia={data.listaViajem[0].distanciaPercorrida + "KM"} 
+              conbustivelRestante={data.listaViajem[0].autonomia + "KM"} 
+              calibragem={data.listaViajem[0].calibragem} 
+              oleoTroca={"Em " + data.listaViajem[0].trocaOleo + " Dias"} />
+
+        <Card title={data.listaViajem[1].title} 
+              kml={data.listaViajem[1].consumoMedio + "KM"} 
+              revisao={"Em "+ data.listaViajem[1].revisao + " Dias"} 
+              distancia={data.listaViajem[1].distanciaPercorrida + "KM"} 
+              conbustivelRestante={data.listaViajem[1].autonomia + "KM"} 
+              calibragem={data.listaViajem[1].calibragem} 
+              oleoTroca={"Em " + data.listaViajem[1].trocaOleo + " Dias"} />
+
+        <Card title={data.listaViajem[2].title} 
+              kml={data.listaViajem[2].consumoMedio + "KM"} 
+              revisao={"Em "+ data.listaViajem[2].revisao + " Dias"} 
+              distancia={data.listaViajem[2].distanciaPercorrida + "KM"} 
+              conbustivelRestante={data.listaViajem[2].autonomia + "KM"} 
+              calibragem={data.listaViajem[2].calibragem} 
+              oleoTroca={"Em " + data.listaViajem[2].trocaOleo + " Dias"} />
+
+
         <FooterDorf />
     </div>
   )
